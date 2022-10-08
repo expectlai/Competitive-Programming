f1faee2946fdd32c4b508ed7c9f8f5be0f22eecb
expectlai <expectlai@gmail.com>
2022-10-08 15:57:53 +0800

    first commit

---
+++ code.cpp
@@ -0,0 +1,10 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0)
+#define ll long long
+using namespace std;
+int main() {
+    fastio;
+
+    return 0;
+}
